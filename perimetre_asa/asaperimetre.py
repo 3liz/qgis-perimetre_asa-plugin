e364a343e28260907981a8132961e8d959f2d46f
Etienne Trimaille <gustrimaille@yahoo.fr>
2021-03-02 12:42:22 +0700

    Fix unload and load the plugin UI, remove extra menu

--- perimetre_asa/asaperimetre.py
+++ perimetre_asa/asaperimetre.py
@@ -5,10 +5,12 @@ __email__ = "info@3liz.org"
 from qgis.core import QgsApplication
 from qgis.utils import iface
 from qgis.PyQt.QtCore import Qt
-from qgis.PyQt.QtWidgets import QMenu
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import QAction
 
 from .dock import AsaPerimetreDock
 from .processing.provider import AsaPerimetreProvider
+from .qgis_plugin_tools.tools.resources import resources_path
 
 
 class AsaPerimetre:
@@ -17,36 +19,50 @@ class AsaPerimetre:
         self.provider = None
         self.menu = None
         self.dock = None
+        self.dock_action = None
 
     def initProcessing(self):
-        """Init Processing provider for QGIS >= 3.8."""
+        """ Init Processing provider for QGIS >= 3.8. """
         self.provider = AsaPerimetreProvider()
         QgsApplication.processingRegistry().addProvider(self.provider)
 
     def initGui(self):
-        """Init the user interface."""
+        """ Init the user interface. """
         self.initProcessing()
         self.dock = AsaPerimetreDock()
 
         iface.addDockWidget(Qt.RightDockWidgetArea, self.dock)
 
-        action = self.dock.toggleViewAction()
+        icon = QIcon(resources_path('icons', 'icon.png'))
 
-        self.menu = QMenu("&Périmètre d'ASA")
+        # Plugin menu
+        self.dock_action = QAction(icon, "Périmètre d'ASA", iface.mainWindow())
+        self.dock_action.triggered.connect(self.manage_dock)
+        iface.pluginMenu().addAction(self.dock_action)
 
-        # Add Perimetre ASA to Extension menu
-        self.menu.addAction(action)
+        # Plugin toolbar
+        iface.addToolBarIcon(self.dock_action)
 
-        plugin_menu = iface.pluginMenu()
-        plugin_menu.addMenu(self.menu)
-        # Add Perimetre ASA toolbar
-        iface.addToolBarIcon(action)
+    def manage_dock(self):
+        """ Open or close the dock. """
+        if self.dock.isVisible():
+            self.dock.close()
+        else:
+            self.dock.show()
+            self.dock.raise_()
 
-    # def unload(self):
-    #     """Unload the plugin."""
-    #     if self.provider:
-    #         QgsApplication.processingRegistry().removeProvider(self.provider)
-    #     iface.removeDockWidget(self.dock)
-    #     iface.removePluginMenu(self.menu)
-    #     iface.removeToolbarIcon(self.toolbar)
-    #     self.dock.deleteLater()
+    def unload(self):
+        """ Unload the plugin. """
+        if self.provider:
+            QgsApplication.processingRegistry().removeProvider(self.provider)
+            del self.provider
+
+        if self.dock_action:
+            iface.pluginMenu().removeAction(self.dock_action)
+            iface.removeToolBarIcon(self.dock_action)
+            del self.dock_action
+
+        if self.dock:
+            iface.removeDockWidget(self.dock)
+            self.dock.deleteLater()
+            del self.dock
